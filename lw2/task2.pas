1e0acd1064758dda3d8ae2f72f694d3286181014
svtln007 <lana_pirogova@vk.com>
2022-03-02 21:54:10 +0300

    лабараторная

---
+++ lw2/task2.pas
@@ -0,0 +1,15 @@
+PROGRAM SarahRevere(INPUT, OUTPUT);
+USES dos;
+BEGIN{SarahRevere}
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  IF GetEnv('QUERY_STRING') = 'lanterns=1'
+  THEN
+    WRITELN('The British are coming by land')
+  ELSE
+    IF GetEnv('QUERY_STRING') = 'lanterns=2'
+    THEN
+      WRITELN('The British are coming by sea')
+    ELSE
+      WRITELN('Sarah didn''t say')
+END.{SarahRevere}
